a49f89c337d90484cc7e860a12f543cf68344de2
kohei kinoshita <aozora0000@gmail.com>
2015-01-13 21:46:23 +0900

    fixed testcase

--- test/testcase.js
+++ test/testcase.js
@@ -1,4 +1,8 @@
+var host = process.env.REDIS_PORT_6379_TCP_ADDR || "127.0.0.1";
+var port = process.env.REDIS_PORT_6379_TCP_PORT || 6379;
+
 var assert = require("assert")
+var client = require('redis').createClient(port, host ,{});
 
 describe('Array', function(){
     describe('#indexOf()', function(){
@@ -6,5 +10,14 @@ describe('Array', function(){
             assert.equal(-1, [1,2,3].indexOf(5));
             assert.equal(-1, [1,2,3].indexOf(0));
         })
+        it('redis connection',function() {
+            setObj = { "status": 200, "message": "hogefuga" };
+            client.set('key:test:0', JSON.stringify(setObj));
+            client.get('key:test;:0',function(err,getObj) {
+                if(err) return console.log(err);
+                assert.equal(setObj,getObj);
+            });
+
+        });
     })
 })
